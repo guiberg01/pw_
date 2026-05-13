e1460cc0c2a9b150a78cf0fa7ea65dea86775d21
Guilherme <guilherme.goncalves01@hotmail.com>
2026-05-13 17:09:52 -0300

    Adicionado funcionalidade para cancelar pedido e confirmar recebimento

--- backend/controllers/order.controller.js
+++ backend/controllers/order.controller.js
@@ -1,16 +1,36 @@
 import { sendSuccess } from "../helpers/successResponse.js";
-import { findOrderByIdForUserOrThrow, listOrdersForUser } from "../services/order.service.js";
+import {
+  findOrderByIdForUserOrThrow,
+  listOrdersForUser,
+  cancelOrderForUserOrThrow,
+  confirmOrderReceivedForUser,
+} from "../services/order.service.js";
 
 export const getMyOrders = async (req, res, next) => {
-    const { page, limit, status, createdFrom, createdTo, sort } = req.validatedQuery ?? {};
-    const orders = await listOrdersForUser(req.user._id, { page, limit, status, createdFrom, createdTo, sort });
+  const { page, limit, status, createdFrom, createdTo, sort } = req.validatedQuery ?? {};
+  const orders = await listOrdersForUser(req.user._id, { page, limit, status, createdFrom, createdTo, sort });
 
-    return sendSuccess(res, 200, "Pedidos listados com sucesso", orders);
+  return sendSuccess(res, 200, "Pedidos listados com sucesso", orders);
 };
 
 export const getMyOrderById = async (req, res, next) => {
-    const { id } = req.params;
-    const order = await findOrderByIdForUserOrThrow(id, req.user._id);
+  const { id } = req.params;
+  const order = await findOrderByIdForUserOrThrow(id, req.user._id);
 
-    return sendSuccess(res, 200, "Pedido encontrado com sucesso", order);
+  return sendSuccess(res, 200, "Pedido encontrado com sucesso", order);
+};
+
+export const cancelMyOrder = async (req, res, next) => {
+  const { id } = req.params;
+  const order = await cancelOrderForUserOrThrow(id, req.user._id);
+
+  return sendSuccess(res, 200, "Pedido cancelado com sucesso", order);
+};
+
+export const confirmMyOrderReceipt = async (req, res, next) => {
+  const { id } = req.params;
+  const { subOrderId = null } = req.validatedBody ?? {};
+  const order = await confirmOrderReceivedForUser(id, req.user._id, { subOrderId });
+
+  return sendSuccess(res, 200, "Pedido confirmado como recebido", order);
 };
